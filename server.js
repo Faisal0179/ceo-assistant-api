89c5e4f9c398a030e3db98b107de701c4f75a72d
Faisal0179 <443101227@student.ksu.edu.sa>
2026-05-16 16:38:28 +0300

    Update server.js

--- server.js
+++ server.js
@@ -4,9 +4,9 @@ const { google } = require("googleapis");
 
 
 const app = express();
+app.use(express.json());
 app.use(express.static("public"));
 app.use(cors());
-app.use(express.json());
 const oauth2Client = new google.auth.OAuth2(
   process.env.GOOGLE_CLIENT_ID,
   process.env.GOOGLE_CLIENT_SECRET,
